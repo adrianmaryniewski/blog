bc5f2ed8db3751c4d3dc2bfa925407bb2643f0c1
“adrianmaryniewski” <adrian@maryniewski.pl>
2023-02-03 18:42:57 +0100

    03/02/2023 9

---
+++ docs/.vitepress/theme/themeConfig.sidebar.js
@@ -0,0 +1,13 @@
+export default {
+    themeConfig: {
+        sidebar: [
+            {
+                text: 'Guide',
+                items: [
+                    { text: 'Introduction', link: '/introduction' },
+                    { text: 'Getting Started', link: '/getting-started' },
+                ]
+            }
+        ]
+    }
+}
