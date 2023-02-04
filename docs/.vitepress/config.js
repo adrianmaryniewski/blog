ef579c13c09152446063ae642ca99716e1761996
“adrianmaryniewski” <adrian@maryniewski.pl>
2023-02-04 15:39:24 +0100

    04/02/2023 8

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,5 @@
 export default {
-    base: '/adrianmaryniewski.github.io/blog/',
+    base: '/adrianmaryniewski.github.io/',
     title: 'maryniewski.pl',
     description: 'The quieter you become, the more you can hear.',
     head: [],
